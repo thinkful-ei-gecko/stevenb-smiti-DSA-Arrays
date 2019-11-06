4cd09e4ed4ed02493e85da55ff39487a4f970750
Steven Bull <stevenwbulljr@gmail.com>
2019-11-06 16:43:21 +0000

    add memory and array classes

---
+++ Array.js
@@ -0,0 +1,31 @@
+import Memory from './Memory';
+
+class Array {
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;
+    this.ptr = Memory.allocate(this.length);
+  }
+
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO)
+    }
+
+    Memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = Memory.allocate(size);
+    if (this.ptr === null) {
+      throw new Error('Out of memory');
+    }
+    Memory.copy(this.ptr, oldPtr, this.length);
+    Memory.free(oldPtr);
+    this._capacity = size;
+  }
+}
+
+Array.SIZE_RATIO = 3;
